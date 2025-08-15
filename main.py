6e0060fee0cf33a2812116086861d8bbe9247ec7
Rajat <24f1002405@ds.study.iitm.ac.in>
2025-08-15 13:42:43 +0530

    Added request-wise debugging info

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 import base64
 import asyncio
 import time
+import random
 
 app = FastAPI()
 app.add_middleware(
@@ -21,10 +22,14 @@ async def home(
     request: Request,
     question_file: UploadFile = File(None, alias="questions.txt")
 ):
+    start_time = time.time()
+    request_id = random.randint(1000, 9999)
+
     # incorrect HTTP method
     if request.method == "GET":
         return {"message": "send data first"}
-    print(f"Received {request.method} request from {request.client.host}")
+    print(f"[{request_id}]: Request ID: {request_id}")
+    print(f"[{request_id}]: Received {request.method} request from {request.client.host}")
 
     # No question file uploaded
     if not question_file:
@@ -59,49 +64,51 @@ async def home(
             
             prompt += file_info + "\n"
 
-        print(f"Files Received: {list(all_files.keys())}")
+        print(f"[{request_id}]: Files Received: {list(all_files.keys())}")
 
     # send llm request
-    print("Sending request to LLM...")
-    response = await asyncio.to_thread(get_llm_response, prompt)
-    print("LLM response received")
+    response = await asyncio.to_thread(get_llm_response, prompt, request_id)
+    print(f"[{request_id}]: LLM response received")
 
+    # execute code and get answers
     local_scope = {"all_files": all_files}
     try:
-        print("Executing code...")
-        exec(clean_python_code(response.text), {}, local_scope)
+        print(f"[{request_id}]: Executing code...")
+        exec(clean_python_code(response.text, request_id), {}, local_scope)
     except Exception as e:
-        print(e)
+        print(f"[{request_id}]: {e}")
         return {"message": "Error executing code"}
     answers = local_scope.get("answers")
     
-    print("Successful response sent back !!!")
+    print(f"[{request_id}]: Successful response sent back in {time.time() - start_time}s !!!")
     return answers
 
-def clean_python_code(code_str: str) -> str:
+def clean_python_code(code_str: str, request_id: int) -> str:
     code_str = code_str.strip()
     if code_str.startswith("```python"):
         code_str = code_str.replace("```python", "").strip()
     if code_str.endswith("```"):
         code_str = code_str[:-3].strip()
     
+    print(code_str)
     return code_str
 
-def get_llm_response(prompt: str, retries: int = 3):
+def get_llm_response(prompt: str, request_id: int, retries: int = 3):
     client = genai.Client()
 
     for attempt in range(1, retries + 1):
         try:
+            print(f"[{request_id}]: Attempt {attempt} to get response from LLM...")
             response = client.models.generate_content(
                 model="gemini-2.5-pro",
                 contents=prompt,
             )
             if not response or not hasattr(response, 'text') or not response.text:
-                raise ValueError("Empty response from LLM")
+                raise ValueError(f"[{request_id}]: Empty response from LLM")
             
             return response
         except Exception as e:
-            print(f"Attempt {attempt} failed with {e}. Retrying in 60s...")
+            print(f"[{request_id}]: Attempt {attempt} failed with {e}. Retrying in 60s...")
             time.sleep(60)
     
-    raise RuntimeError(f"Failed to get a valid response from the LLM after {retries} attempts")
+    raise RuntimeError(f"[{request_id}]: Failed to get a valid response from the LLM after {retries} attempts")
